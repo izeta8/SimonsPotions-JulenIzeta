e4625706b6c8b15e5dd513d89b837a76767b7872
izeta8 <julen.izeta@ikasle.aeg.eus>
2024-09-16 18:27:11 +0200

    Implement the simon's game tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,186 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
+import useSound from 'use-sound';
+import simon from './assets/sounds/sprite.mp3'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const blueRef   = useRef(null);
+  const yellowRef = useRef(null);
+  const greenRef  = useRef(null);
+  const redRef    = useRef(null);
+
+  const [play] = useSound(simon, {
+    sprite: {
+      one:   [0,    500],
+      two:   [1000, 500],
+      three: [2000, 500],
+      four:  [3000, 500],
+      error: [4000, 1000],
+    }
+  });
+
+  const initGame = () => {
+    randomNumber();
+    setIsGameOn(true);
+  }
+
+  const randomNumber = () => {
+    setIsAllowedToPlay(false);
+    const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    setSequence([...sequence, randomNumber]);
+    setTurn(turn + 1);
+  }
+
+  const handleClick = (index) => {
+    if (isAllowedToPlay) {
+      setIsAllowedToPlay(false); // Prevent further clicks
+      play({ id: colors[index].sound });
+      colors[index].ref.current.style.opacity = 1;
+      colors[index].ref.current.style.scale = 0.9;
+      setTimeout(() => {
+        colors[index].ref.current.style.opacity = 0.5;
+        colors[index].ref.current.style.scale = 1;
+        setCurrentGame([...currentGame, index]);
+        setPulses(pulses + 1);
+        setIsAllowedToPlay(true); // Re-allow clicks after animation
+      }, speed / 2);
+    }
+  };
+
+  const colors = [
+    {
+      color: '#FAF303',
+      ref: yellowRef,
+      sound: 'one'
+    },
+    {
+      color: '#030AFA',
+      ref: blueRef,
+      sound: 'two'
+    },
+    {
+      color: '#FA0E03',
+      ref: redRef,
+      sound: 'three'
+    },
+    {
+      color: '#0AFA03',
+      ref: greenRef,
+      sound: 'four'
+    }
+  ];
+
+  const minNumber = 0;
+  const maxNumber = 3;
+  const speedGame = 400;
+
+  const [sequence, setSequence] = useState([]);
+  const [currentGame, setCurrentGame] = useState([]);
+  const [isAllowedToPlay, setIsAllowedToPlay] = useState(false);
+  const [speed, setSpeed] = useState(speedGame);
+  const [turn, setTurn] = useState(0);
+  const [pulses, setPulses] = useState(0);
+  const [success, setSuccess] = useState(0);
+  const [isGameOn, setIsGameOn] = useState(false);
+
+
+  useEffect(() => {
+    if (pulses > 0) {
+      if (Number(sequence[pulses - 1]) === Number(currentGame[pulses - 1])) {
+        setSuccess(success + 1);
+      } else {
+        const index = sequence[pulses - 1];
+        if (index) colors[index].ref.current.style.opacity = (1);
+        play({ id: 'error' });
+        setTimeout(() => {
+          if (index) colors[index].ref.current.style.opacity = (0.5);
+          setIsGameOn(false);
+        }, speed * 2);
+        setIsAllowedToPlay(false);
+      }
+    }
+  }, [pulses]);
+
+  useEffect(() => {
+    if (!isGameOn)
+    {
+      setSequence([]);
+      setCurrentGame([]);
+      setIsAllowedToPlay(false);
+      setSpeed(speedGame);
+      setSuccess(0);
+      setPulses(0);
+      setTurn(0);
+    }
+  }, [isGameOn]);
+
+  useEffect(() => {
+    if (success === sequence.length && success > 0)
+    {
+      setSpeed(speed - sequence.length * 2);
+      setTimeout(() => {
+        setSuccess(0);
+        setPulses(0);
+        setCurrentGame([]);
+        randomNumber();
+      }, 500); 
+    }
+  }, [success]);
+
+  useEffect(() => {
+    if (!isAllowedToPlay) {
+      sequence.map((item, index) => {
+        setTimeout(() => {
+          play({ id: colors[item].sound });
+          colors[item].ref.current.style.opacity = 1;
+          setTimeout(() => {
+            colors[item].ref.current.style.opacity = 0.5;
+          }, speed / 2);
+        }, speed * index);
+      });
+    }
+    setIsAllowedToPlay(true);
+  }, [sequence]);
+  
+  useEffect(() => {
+    console.log(isAllowedToPlay);
+  }, [isAllowedToPlay])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isGameOn
+      ? (
+        <>
+          <div className='header'>
+            <h1>Turn {turn}</h1>
+          </div>
+          <div className='container'>
+            {colors.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  ref={item.ref}
+                  className={`pad pad-${index}`}
+                  style={{backgroundColor: `${item.color}`, opacity:0.6}}
+                  onClick={() => handleClick(index)}
+                />
+              )
+            })}
+          </div>
+        </>
+      )
+      : (
+        <>
+          <div className='header'>
+            <h1>SUPER SIMON</h1>
+          </div>
+          <button onClick={initGame}>START</button>
+        </>
+      )}
     </>
   )
+
 }
 
 export default App
